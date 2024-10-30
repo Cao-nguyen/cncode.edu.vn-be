04bbc01af3ab9ce597f7d42b433a97c498517d80
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-10-30 18:15:30 +0700

    o

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Chạy dự án ReactJs với Lý Cao Nguyên
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
